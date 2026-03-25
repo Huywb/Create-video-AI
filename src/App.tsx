483a66140144e6e1724d5940e9ef9a24ca3058a5
huywb <pthhuy1306@gmail.com>
2026-03-25 16:00:44 +0700

    feature/setup router and mock data for app

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,30 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import SoftBackdrop from './components/SoftBackdrop';
 import Footer from './components/Footer';
-import LenisScroll from './components/lenis';
+import { Route, Routes } from 'react-router-dom';
+import Plans from './pages/Plans';
+import Community from './pages/Community';
+import Generator from './pages/Generator';
+import Result from './pages/Result';
+import Loading from './pages/Loading';
+import MyGenerations from './pages/MyGenerations';
 
 function App() {
 	return (
 		<>
 			<SoftBackdrop />
 			<Navbar />
-			<Home />
+
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/generate' element={<Generator />} />
+				<Route path='/community' element={<Community />} />
+				<Route path='/plans' element={<Plans />} />
+				<Route path='/loading' element={<Loading />} />
+				<Route path='/result/:projectId' element={<Result />} />
+				<Route path='/my-generations' element={<MyGenerations />} />
+			</Routes>
+
 			<Footer />
 		</>
 	);
