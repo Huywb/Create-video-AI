d99bf7e3d1afe8830c03ba112fefebe7f6c3c5ad
huywb <pthhuy1306@gmail.com>
2026-03-29 08:42:56 +0700

    FE-Feature/Connect logic BE for MyGenerations components

--- frontend/src/pages/MyGenerations.tsx
+++ frontend/src/pages/MyGenerations.tsx
@@ -4,21 +4,49 @@ import { dummyGenerations } from '../assets/assets'
 import { Loader2Icon } from 'lucide-react'
 import ProjectCard from '../components/ProjectCard'
 import { PrimaryButton } from '../components/Buttons'
+import { useAuth, useUser } from '@clerk/react'
+import { useNavigate } from 'react-router-dom'
+import api from '../config/axios'
+import toast from 'react-hot-toast'
 
 const MyGenerations = () => {
+
+  const {user,isLoaded} = useUser()
+  const {getToken} = useAuth()
+  const navigate = useNavigate()
   const [generations, setGenerations] = useState<Project[]>([])
   const [loading, setLoading] = useState(true)
 
   const fetchGenerations = async () => {
-    setTimeout(() => {
-      setGenerations(dummyGenerations)
+    try {
+      const token = await getToken()
+      console.log(2)
+      const res = await api.get('/api/v1/user/projects',{
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      console.log(res.data.data)
+      if(res.data.data<19){
+      setGenerations(res.data.data.projects)
+      }else {
+        setGenerations(dummyGenerations)
+      }
       setLoading(false)
-    }, 3000)
+    } catch (error : any) {
+      toast.error(error.message)
+      console.log(error.message)
+      
+    }
 
   }
 
   useEffect(() => {
-    fetchGenerations()
+    if(user){
+      fetchGenerations()
+    }else if(isLoaded && !user){
+      navigate('/')
+    }
   }, [])
 
   return loading ? (
@@ -37,7 +65,7 @@ const MyGenerations = () => {
         </header>
 
         <div className="columns-1 sm:columns-2 lg:columns-3 gap-4">
-          {generations.map(generation => (
+          {generations?.map(generation => (
             <div>
               <ProjectCard key={generation.id} gen={generation} setGenerations={setGenerations} forCommunity={false}></ProjectCard>
             </div>
